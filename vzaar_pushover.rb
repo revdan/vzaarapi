02dc5c94c39aec2bdc64f8920cf6742b4f5a7b74
Dan Rees-Jones <dan@vzaar.com>
2012-12-14 14:53:24 +0000

    added mail and rushover gem

--- vzaar_pushover.rb
+++ vzaar_pushover.rb
@@ -77,11 +77,35 @@ class VzaarPushover < Sinatra::Base
       from     'dan@vzaar.com'
       to       'dan@vzaar.com'
       subject  'Video Uploaded!'
-      body     "#{request.env["rack.input"].read}"
+      body     "#{dump}"
     end
 
     mail.delivery_method :sendmail
 
     mail.deliver
   end
-end
+  
+  def dump
+    request.body              # request body sent by the client (see below)
+    request.scheme            # "http"
+    request.script_name       # "/example"
+    request.path_info         # "/foo"
+    request.port              # 80
+    request.request_method    # "GET"
+    request.query_string      # ""
+    request.content_length    # length of request.body
+    request.media_type        # media type of request.body
+    request.host              # "example.com"
+    request.get?              # true (similar methods for other verbs)
+    request.form_data?        # false
+    request["SOME_HEADER"]    # value of SOME_HEADER header
+    request.referer           # the referer of the client or '/'
+    request.user_agent        # user agent (used by :agent condition)
+    request.cookies           # hash of browser cookies
+    request.xhr?              # is this an ajax request?
+    request.url               # "http://example.com/example/foo"
+    request.path              # "/example/foo"
+    request.ip                # client IP address
+    request.secure?           # false
+    request.env               # raw env hash handed in by Rack
+  end
