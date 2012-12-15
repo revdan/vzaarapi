52d88f963c3574691f775378dc4322179ad25208
Dan <revdan@gmail.com>
2012-12-15 10:38:03 +0000

    disabled upload button on load

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,45 @@
 jQuery(function($) {
+		
 	$('input[type=file]').change(function(e){
+		
+		$('#submit').removeAttr('disabled');
+		
 	  var filename = $(this).val().split('\\').pop();
 	  $('#upload_label').html(filename);
+		
+		//var vid = getUrlVars()["video"];
+		//var timer = checkStatus(vid);
 	});
 });
+
+/*
+var checkStatus = function (vid) {
+	$.ajax({
+	  type: "POST",
+	  url: "details.php",
+	  data: { video: vid }
+	}).done(function( data ) {
+		console.log(data);
+		if(data.match(/unavailable/)) {
+	  	$('#wrapper').html('<img src="http://placehold.it/768x432/000000/eeeeee&text=video+not+found" />');	
+			clearTimeout(timer);
+  	} 
+		else if(data.match(/available/)) {
+			$('#wrapper').html('<iframe allowFullScreen allowTransparency="true" class="vzaar-video-player" frameborder="0" height="432" id="vzvd-' + vid + '" name="vzvd-' + vid + '" src="http://view.vzaar.com/' + vid + '/player" title="vzaar video player" type="text/html" width="768"></iframe>');
+			clearTimeout(timer);
+  	} 
+		else if(data.match(/failed/)) {
+			$('#wrapper').html('<img src="http://placehold.it/000000/eeeeee768x432&text=video+encoding+failed" />');
+			clearTimeout(timer);
+  	} 
+		else if (data.match(/processing/)){
+  		$('#wrapper').html('<img src="http://placehold.it/000000/eeeeee768x432&text=video+processing" />');
+			var timer = setTimeout(function(){checkStatus(vid)}, 10000); // check again in 10 secs
+	  } 
+		else {
+	  	$('#wrapper').html('<img src="http://placehold.it/000000/eeeeee768x432&text=video+not+found" />');	
+			clearTimeout(timer);
+	  }
+	});	
+}
+*/
