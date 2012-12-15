afb7b1114efc6df3f8de68139d008c80ce30702b
Dan <revdan@gmail.com>
2012-12-15 08:19:50 +0000

    cut out some uselessness

--- vzaar_pushover.rb
+++ vzaar_pushover.rb
@@ -94,36 +94,4 @@ class VzaarPushover < Sinatra::Base
     
   end
   
-  get '/thelisteningtree?' do
-    #filename = "//#{DOMAIN}/test.xml"
-    #file_content = File.read(filename)
-    @xmldoc = Nokogiri::XML('{"<?xml version"=>"\"1.0\" encoding=\"UTF-8\"?>\n<vzaar-api>\n  <video>\n    <title>api test 2012-12-14 17:14:59  0000 - mythtvexample.nuv</title>\n    <description nil=\"true\"></description>\n    <state>ready</state>\n    <url>http://vzaar.com/videos/1137850</url>\n    <id>1137850</id>\n  </video>\n</vzaar-api>\n"}')
-    @state = @xmldoc.xpath("//state")
-    slim :the_listening_tree
-  end
-  
-  def dump
-    request.body              # request body sent by the client (see below)
-    request.scheme            # "http"
-    request.script_name       # "/example"
-    request.path_info         # "/foo"
-    request.port              # 80
-    request.request_method    # "GET"
-    request.query_string      # ""
-    request.content_length    # length of request.body
-    request.media_type        # media type of request.body
-    request.host              # "example.com"
-    request.get?              # true (similar methods for other verbs)
-    request.form_data?        # false
-    request["SOME_HEADER"]    # value of SOME_HEADER header
-    request.referer           # the referer of the client or '/'
-    request.user_agent        # user agent (used by :agent condition)
-    request.cookies           # hash of browser cookies
-    request.xhr?              # is this an ajax request?
-    request.url               # "http://example.com/example/foo"
-    request.path              # "/example/foo"
-    request.ip                # client IP address
-    request.secure?           # false
-    request.env               # raw env hash handed in by Rack
-  end
 end
