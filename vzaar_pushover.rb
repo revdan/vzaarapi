012fe6d70405a56a16c47309db5f6fb1f8225093
Dan <revdan@gmail.com>
2012-12-15 12:51:55 +0000

    i'm stupid

--- vzaar_pushover.rb
+++ vzaar_pushover.rb
@@ -22,6 +22,16 @@ class VzaarPushover < Sinatra::Base
     DOMAIN = "localhost:9292"
   end
   
+  Mongoid.configure do |config|
+    if ENV['MONGOHQ_URL']
+      conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+      uri = URI.parse(ENV['MONGOHQ_URL'])
+      config.master = conn.db(uri.path.gsub(/^\//, ''))
+    else
+      config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('test')
+    end
+  end
+  
   v = VzaarInit.new
   client = Rushover::Client.new('qs9jDTdWKjscFDAe5CdapqYA3aC4qn')
   
