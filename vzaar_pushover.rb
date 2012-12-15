9d3fbfa6a527eb64b6d1cbe81137c8199dc1a8f3
Dan <revdan@gmail.com>
2012-12-15 13:15:47 +0000

    try again

--- vzaar_pushover.rb
+++ vzaar_pushover.rb
@@ -22,15 +22,7 @@ class VzaarPushover < Sinatra::Base
     DOMAIN = "localhost:9292"
   end
   
-  Mongoid.configure do |config|
-    if ENV['MONGOHQ_URL']
-      conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-      uri = URI.parse(ENV['MONGOHQ_URL'])
-      config.master = conn.db(uri.path.gsub(/^\//, ''))
-    else
-      config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('test')
-    end
-  end
+  Mongoid.load!("./mongoid.yml", :production)
   
   v = VzaarInit.new
   client = Rushover::Client.new('qs9jDTdWKjscFDAe5CdapqYA3aC4qn')
